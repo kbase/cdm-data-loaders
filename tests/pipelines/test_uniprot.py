e7527edb5fb00aeec8a3de0b39a5bce9fa21ed7e
ialarmedalien <ialarmedalien@gmail.com>
2026-04-09 15:06:57 -0700

    Fixing invalid param

--- tests/pipelines/test_uniprot.py
+++ tests/pipelines/test_uniprot.py
@@ -102,10 +102,11 @@ def test_cli_all_variants(input_dir: str, destination: str, start_at: str, outpu
     assert s.output == "/some/dir"
 
 
-def test_cli_invalid_destination_via_cli_raises() -> None:
+@pytest.mark.parametrize("bad", ["gcs", "filesystem", "", "LocalFs"])
+def test_cli_invalid_destination_via_cli_raises(bad: str) -> None:
     """Ensure that an invalid destination passed via CLI raises an error."""
     with pytest.raises(ValidationError, match="Value error, destination must be one of"):
-        CliApp.run(Settings, cli_args=["--destination", "s3"])
+        CliApp.run(Settings, cli_args=["--destination", bad])
 
 
 def test_cli_passes_settings_class_to_run_cli() -> None:
